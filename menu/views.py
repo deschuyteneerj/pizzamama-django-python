765017160cc6f7534a355280e88d5178c10c7b26
Jonathan <deschuyteneerj@gmail.com>
2021-09-22 16:37:51 +0200

    remove some commented code

--- menu/views.py
+++ menu/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Pizza
 
 
 # Create your views here.
 def index(request):
-    """pizzas = Pizza.objects.all()
-    pizzas_names_and_prices = [pizza.name + ' : ' + str(pizza.price) + '€' for pizza in pizzas]
-    pizzas_names_and_prices_str = ', '.join(pizzas_names_and_prices)
-    return HttpResponse('Pizzas: ' + pizzas_names_and_prices_str)"""
     pizzas = Pizza.objects.all().order_by('price')
     return render(request, 'menu/index.html', {'pizzas': pizzas})
